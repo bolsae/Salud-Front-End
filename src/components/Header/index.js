d40709d38ee88bb3a10d5c6f8f1d312b78ae6ec3
thiagofranco85 <thiago.boo@gmail.com>
2024-11-05 14:55:00 -0300

    adiciona botão de contato via WhatsApp no cabeçalho e estiliza a exibição

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react'
+import config from '../../config'
 
 import Navbar from '../Navbar'
 
@@ -35,7 +36,7 @@ export default class Header extends Component
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </section>                
                 </header>
                 <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 289.83">
                     <linearGradient id="bg-svg-topo-header" x2="1" y2="0">
@@ -45,6 +46,10 @@ export default class Header extends Component
                     <path fill="url(#bg-svg-topo-header)" stroke="none"
                     d="M1920,74.7s-534.19,194.41-721.38,210C538.28,339.7-1,74.7-1,74.7V-.3H1920Z" />
                 </svg>
+
+                <a id='ligaTag' class="btn btn-lg btn-default abaClicavel" href={`https://web.whatsapp.com/send?phone=55${config.WHATSAPP}`} target="_blank" role="button" rel="noreferrer">
+                    <div id="liga">Fale pelo Whatsapp</div>
+                </a>
             </Fragment>
         )
     }
